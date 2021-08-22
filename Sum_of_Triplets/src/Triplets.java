2d7bec9d62a5b608b9a3fd118755c5ca2f67043c
dhruvshetty0103 <dhruvshetty0103@gmail.com>
2021-08-22 19:45:03 +0530

    Added:Functional Program 2:Sum of triplets and the number of triplets in array to be displayed

--- Sum_of_Triplets/src/Triplets.java
+++ Sum_of_Triplets/src/Triplets.java
@@ -12,18 +12,18 @@ public class Triplets
 		for(i=0;i<n;i++)
 			arr[i]=reader.nextInt();
 		reader.close();
-		for(i=0;i<n-2;i++)
-			for(j=i+1;j<n-1;j++)
-				for(k=j+1;k<n;k++)
+		for(i=0;i<n-2;i++)//Iterate till last 3rd element
+			for(j=i+1;j<n-1;j++)//Iterate till last 2nd element
+				for(k=j+1;k<n;k++)//Iterate till last element
 				{
-					if(arr[i]+arr[j]+arr[k]==0)
+					if(arr[i]+arr[j]+arr[k]==0)//Checking whether sum of three numbers in a array gives a triplet or not
 					{
 						flag=1;
 						count++;
 						System.out.println("Triplets are:"+arr[i]+","+arr[j]+","+arr[k]);
 					}
 				}
-		if(flag==1)
+		if(flag==1)//If any triplets exists
 			System.out.println("Total number of triplets are:"+count);
 		else
 			System.out.println("No triplets found.");
