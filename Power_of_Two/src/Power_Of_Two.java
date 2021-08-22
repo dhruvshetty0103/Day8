43b0d53a34237abae714d5b73b016e659e4ba34a
dhruvshetty0103 <dhruvshetty0103@gmail.com>
2021-08-22 11:40:19 +0530

    Modified:Updated code to implement command line interface

--- Power_of_Two/src/Power_Of_Two.java
+++ Power_of_Two/src/Power_Of_Two.java
@@ -1,24 +1,17 @@
-import java.util.*;
-public class Power_Of_Two {
-
+public class Power_Of_Two 
+{
 	public static void main(String[] args) 
 	{
-		int i,N;
-		System.out.println("Enter value of N to print a table of the powers of 2 that are less than 31");
-		Scanner reader=new Scanner(System.in);
-		N=reader.nextInt();
-		reader.close();
-		
-		if(N>=0 && N<31)
+		double N;			
+		N=Double.parseDouble(args[0]);//To read N from command line interface
+		if(N>=0 && N<31)//0<=N<31
 		{
-			for(i=0;i<=N;i++)
+			for(int i=0;i<=N;i++)
 			{
-				System.out.println(Math.pow(2, i));
+				System.out.println(Math.pow(2, i));//Math.pow used generate 2^N
 			}
 		}
 		else
 			System.out.println("Value less than or greater than equal to 31 are not allowed");
-		
 	}
-
 }
